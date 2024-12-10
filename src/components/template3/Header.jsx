ecbd3a167187f9d054cceafb1321e2f27b0f6758
zafar-m3n <thezafar.m3n@gmail.com>
2024-12-10 14:20:39 +0530

    Responsive Header

--- src/components/template3/Header.jsx
+++ src/components/template3/Header.jsx
@@ -1,49 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "@/assets/logo.png";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className="flex items-center justify-between p-4 border-b px-12 lg:px-20 bg-white">
-      <div className="flex items-center space-x-6">
-        <div className="flex items-center">
-          <a href="/">
-            <img src={logo} alt="Logo" className="h-10 w-auto" />
-          </a>
+    <nav className="bg-white border-gray-200 border-b-2">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="/" className="flex items-center space-x-3">
+          <img src={logo} className="h-8" alt="Logo" />
+        </a>
+        <div className="flex md:order-2 space-x-3 md:space-x-0">
+          <button
+            type="button"
+            className="text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 text-center"
+          >
+            Apply
+          </button>
+          <button
+            data-collapse-toggle="navbar-cta"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            aria-controls="navbar-cta"
+            aria-expanded={isMenuOpen ? "true" : "false"}
+            onClick={toggleMenu}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
         </div>
-        <nav>
-          <ul className="flex space-x-4">
+        <div
+          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+            isMenuOpen ? "block" : "hidden"
+          }`}
+          id="navbar-cta"
+        >
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white">
             <li>
-              <a href="#" className="hover:text-gray-300">
-                Who We Are
+              <a
+                href="#"
+                className="block py-2 px-3 md:p-0 text-white bg-blue-500 rounded md:bg-transparent md:text-blue-500"
+              >
+                Our Values
               </a>
             </li>
             <li>
-              <a href="#" className="hover:text-gray-300">
-                What We Offer
+              <a
+                href="#"
+                className="block py-2 px-3 md:p-0 text-black hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-500"
+              >
+                Interview Process
               </a>
             </li>
             <li>
-              <a href="#" className="hover:text-gray-300">
-                Our Careers
+              <a
+                href="#"
+                className="block py-2 px-3 md:p-0 text-black hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-500"
+              >
+                Available Positions
               </a>
             </li>
             <li>
-              <a href="#" className="hover:text-gray-300">
-                How To Apply
+              <a
+                href="#"
+                className="block py-2 px-3 md:p-0 text-black hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-500"
+              >
+                Testimonials
               </a>
             </li>
           </ul>
-        </nav>
+        </div>
       </div>
-      <div>
-        <a
-          href="#"
-          className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600 transition"
-        >
-          Apply
-        </a>
-      </div>
-    </header>
+    </nav>
   );
 };
 
